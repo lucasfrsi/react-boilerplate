6c1f06b7e4fc52241baf5b888d1e5e982b7d2388
Lucas F. Simoni <lucasfrsi@gmail.com>
2021-04-21 09:50:53 -0300

    Remove styled-components test style

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import styled from 'styled-components';
-
-const StyledNavLink = styled(NavLink)`
-  color: red;
-
-  &.active {
-    color: blue;
-  }
-`;
 
 const NavBar = () => (
   <div>
-    <StyledNavLink exact to="/">Home</StyledNavLink>
-    <StyledNavLink to="/posts">Posts</StyledNavLink>
+    <NavLink exact to="/">Home</NavLink>
+    <NavLink to="/posts">Posts</NavLink>
   </div>
 );
 
